de05267e7d3ee1c17ae7763bad15d83ed4bf31b1
ajmager <61753697+ajmager@users.noreply.github.com>
2022-02-17 14:10:29 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -21,27 +21,27 @@ module alu(
            ALU_Result <= {zeros[31:0], A - B};
         2 : // Multiplication
 			  ALU_Result <= tempMulOut;
-        4 : // Division
+        3 : // Division
 			  ALU_Result <= tempDivOut;
-        5 : // AND
+        4 : // AND
            ALU_Result <= {zeros[31:0], A & B};
-        6 : // OR
+        5 : // OR
            ALU_Result <= {zeros[31:0], A | B};
-        7 : // shift left
+        6 : // shift left
            ALU_Result <= {zeros[31:0], B<<1};
-        8 : // shift right
+        7 : // shift right
            ALU_Result <= {zeros[31:0], B>>1}; 
-        9 : // rotate left
+        8 : // rotate left
            ALU_Result <= {zeros[31:0], {B[30:0],B[31]}};
-        10: // rotate right
+        9: // rotate right
            ALU_Result <= {zeros[31:0], {B[0],B[31:1]}};
-        11 : // negate
+        10 : // negate
            ALU_Result <= {zeros[31:0],~B}; 
-        12 : // XOR   
+        11 : // XOR   
            ALU_Result <= {zeros[31:0], A ^ B}; 
-        13 : // NOR
+        12 : // NOR
            ALU_Result <= {zeros[31:0], ~(A | B)};
-        14 : //NAND
+        13 : //NAND
            ALU_Result <= {zeros[31:0], ~(A & B)};
                            
           default: ALU_Result = 64'bx ; 
