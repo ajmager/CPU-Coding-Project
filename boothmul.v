ead97126d2a5fa55b5855b92419b72aef1f1e8e1
PrometheusNavagadro <59888414+PrometheusNavagadro@users.noreply.github.com>
2022-02-21 17:04:26 -0500

    updates

--- boothmul.v
+++ boothmul.v
@@ -1,27 +1,29 @@
 module boothmul(X, Y, Z);
 	input signed [31:0] X, Y;
-	output signed [63:0] Z;
+	output signed[63:0] Z;
+	reg [31:0] tempZ;
 	reg [1:0] temp;
 	integer i;
 	reg E1;
 	reg [31:0] Y1;
 	always @ (X, Y)
 	begin
-		Z = 64'd0;
+		tempZ = 64'd0;
 		E1 = 1'd0;
 		Y1 = ~Y;
-		Z[63:0]=X;
+		tempZ[63:0]=X;
 		for (i = 0; i < 32; i = i + 1)
 		begin
 			temp = {X[i], E1};
 			case (temp)
-				2'd2 : Z [63 : 32] = Z [63 : 32] + Y1;
-				2'd1 : Z [63 : 32] = Z [63 : 32] + Y;
+				2'd2 : tempZ [63 : 32] = tempZ [63 : 32] + Y1;
+				2'd1 : tempZ [63 : 32] = tempZ [63 : 32] + Y;
 				default : begin end
 			endcase
-			Z = Z >> 1;
-			Z[63] = Z[62];
+			tempZ = tempZ >> 1;
+			tempZ[63] = tempZ[62];
 			E1 = X[i];
 		end
 	end
+	assign Z = tempZ;
 endmodule
