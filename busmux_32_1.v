56ccd5125b810181654a5d83fb6668e42d541fc7
PrometheusNavagadro <59888414+PrometheusNavagadro@users.noreply.github.com>
2022-02-21 15:57:15 -0500

    syntax updates

--- busmux_32_1.v
+++ busmux_32_1.v
@@ -1,10 +1,10 @@
 //32:1 BusMux
 
 module BusMux_32_1(
-output	[31:0]	mux_out,
+output reg	[31:0]	mux_out,
 input		[31:0]	BusMuxIn_R0, BusMuxIn_R1, BusMuxIn_R2, BusMuxIn_R3, BusMuxIn_R4, BusMuxIn_R5, BusMuxIn_R6, BusMuxIn_R7, BusMuxIn_R8, BusMuxIn_R9, BusMuxIn_R10, BusMuxIn_R11, BusMuxIn_R12, BusMuxIn_R13, BusMuxIn_R14, BusMuxIn_R15,
 input		[31:0]	BusMuxIn_HI, BusMuxIn_LO,
-input		[31:0]	BusMuxIn_ZHI, BUSMuxIn_ZLOW,
+input		[31:0]	BusMuxIn_ZHI, BusMuxIn_ZLOW,
 input 	[31:0]	BusMuxIn_PC,
 input		[31:0]	BusMuxIn_MDR,
 input		[31:0]	BusMuxIn_InPort,
@@ -15,7 +15,8 @@ input		[4:0]		select
 always @ (BusMuxIn_R0, BusMuxIn_R1, BusMuxIn_R2, BusMuxIn_R3, BusMuxIn_R4, 
 BusMuxIn_R5, BusMuxIn_R6, BusMuxIn_R7, BusMuxIn_R8, BusMuxIn_R9, BusMuxIn_R10, 
 BusMuxIn_R11, BusMuxIn_R12, BusMuxIn_R13, BusMuxIn_R14, BusMuxIn_R15, BusMuxIn_HI,
-BusMuxIn_LO, BusMuxIn_MDR, BusMuxIn_InPort, C_sign_extended, select) begin
+BusMuxIn_LO, BusMuxIn_ZHI, BusMuxIn_ZLOW, BusMuxIn_PC, BusMuxIn_MDR, BusMuxIn_InPort,
+C_sign_extended, select) begin
 
 case(select)
 0:			mux_out = BusMuxIn_R0;
