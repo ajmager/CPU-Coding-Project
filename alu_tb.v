744cb3cb5a2ca7a01226622efb6f1b6da0fa6085
ajmager <61753697+ajmager@users.noreply.github.com>
2022-02-24 21:42:04 -0800

    Update alu_tb.v

--- alu_tb.v
+++ alu_tb.v
@@ -3,35 +3,32 @@
 module alu_tb;
 
 reg [31:0] A, B;
-reg [4:0] ALU_Select;
+reg [3:0] ALU_Sel;
 reg clk; 
 
 wire [63:0] ALU_Out;
-//wire CarryOut; 
 
 integer i; 
 
 alu test(
 	A,B,
-	ALU_Select,
-	ALU_Out
-	//CarryOut	
+	clk,
+	ALU_Sel,
+	ALU_Out	
 );
 initial begin
-	A <= 8'h0A;
-	B <= 4'h02;
-	ALU_Select <= 4'h0;
+	A <= 8'h22;
+	B <= 8'h24;
+	ALU_Sel <= 4'b0100;
 	
 	for (i = 0; i <= 13; i = i + 1)
 	begin	
-		ALU_Select <= ALU_Select + 8'h01;
+		//ALU_Sel <= ALU_Sel + 4'b0001;
 		#10; 
-	end;
+	end
 	
 	A <= 8'hF6;
 	B <= 8'h0A;
 
 end
 endmodule
-	
-	
