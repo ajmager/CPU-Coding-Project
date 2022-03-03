0276814b9a2ddf7c4adff0a4c4c69fc9985e36ec
ajmager <61753697+ajmager@users.noreply.github.com>
2022-03-03 15:27:59 -0500

    working booth mul

--- boothmul.v
+++ boothmul.v
@@ -4,17 +4,17 @@ module boothmul(X, Y, Z);
 	reg [63:0] tempZ;
 	reg [1:0] temp;
 	integer i;
-	reg E1;
+	//reg E1;
 	reg [31:0] Y1;
 	always @ (X, Y)
 	begin
 		tempZ = 64'd0;
-		E1 = 1'd0;
+		//E1 = 1'd0;
 		Y1 = ~Y;
 		tempZ[63:0]=X;
 		for (i = 0; i < 32; i = i + 1)
 		begin
-			temp = {X[i], E1};
+			temp = X[i];
 			case (temp)
 				2'd2 : tempZ [63 : 32] = tempZ [63 : 32] + Y1;
 				2'd1 : tempZ [63 : 32] = tempZ [63 : 32] + Y;
@@ -22,7 +22,7 @@ module boothmul(X, Y, Z);
 			endcase
 			tempZ = tempZ >> 1;
 			tempZ[63] = tempZ[62];
-			E1 = X[i];
+			//E1 = X[i];
 		end
 	end
 	assign Z = tempZ;
