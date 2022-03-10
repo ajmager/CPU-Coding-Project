5f8f0ab3b2e1060f8a6e025ff909cd5bd3ab6411
ajmager <61753697+ajmager@users.noreply.github.com>
2022-03-10 15:31:34 -0500

    initial r0 module

--- register_r0.v
+++ register_r0.v
@@ -1,1 +1,25 @@
+//register for r0
 
+module register_r0(
+
+	input			clk, clr, enable,
+  input			[31:0]	input_D32, input BAout,
+  output reg [31:0] output_r0
+);
+  
+  wire [31:0]	output_Q32; 
+  wire [31:0] BAtemp;
+  
+always @ (posedge clk) begin
+
+if (clr) begin
+	output_r0 <= 32'h00000000;
+end
+else if (enable) begin
+	output_Q32 <= input_D32;
+  BAtemp <= ~BAout;
+  output_r0 <= BAtemp & output_Q32;
+end
+
+end
+endmodule
