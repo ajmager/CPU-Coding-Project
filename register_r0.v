3927fa30fd96352d702f250ce15d49984ca3d8c8
ajmager <61753697+ajmager@users.noreply.github.com>
2022-03-10 15:32:45 -0500

    Update register_r0.v

--- register_r0.v
+++ register_r0.v
@@ -2,9 +2,9 @@
 
 module register_r0(
 
-	input			clk, clr, enable,
-  input			[31:0]	input_D32, input BAout,
-  output reg [31:0] output_r0
+	input	clk, clr, enable,
+  	input	[31:0]	input_D32, input BAout,
+  	output reg [31:0] output_r0
 );
   
   wire [31:0]	output_Q32; 
@@ -13,12 +13,12 @@ module register_r0(
 always @ (posedge clk) begin
 
 if (clr) begin
-	output_r0 <= 32'h00000000;
+	output_Q32 <= 32'h00000000;
 end
 else if (enable) begin
 	output_Q32 <= input_D32;
-  BAtemp <= ~BAout;
-  output_r0 <= BAtemp & output_Q32;
+  	BAtemp <= ~BAout;
+  	output_r0 <= BAtemp & output_Q32;
 end
 
 end
